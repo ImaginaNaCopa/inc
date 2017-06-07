d41ebfe7bdd2aca4526bf0cf015a32b702a3afac
Jonathan Moraes <arkyebr@gmail.com>
2017-06-07 19:42:13 -0300

    Set Continuous Integration

--- InC-environment.rb
+++ InC-environment.rb
@@ -1,14 +1,9 @@
 execute 'apt-get update'
 ## Libary SDL2
 execute 'apt-get install -y --force-yes libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev libsdl2-mixer-dev'
-## Git for clone
-execute 'apt-get install -y --force-yes git'
 ## Clone project
-execute 'git clone https://github.com/ImaginaNaCopa/InC.git'
-execute 'cd InC'
-## Build essential
 execute 'apt-get -y --force-yes install build-essential'
 ## Cmake for build
 execute 'apt-get -y --force-yes install cmake'
 ## Set compiler g++ for c++ (files *.cpp)
-execute 'cmake -DCMAKE_CXX_COMPILER=$(which g++) -DCMAKE_C_COMPILER=$(which gcc) -DCMAKE_INSTALL_PREFIX:PATH=$(readlink -e $(pwd)/..) ..'
+execute 'cmake -DCMAKE_CXX_COMPILER=$(which g++) -DCMAKE_C_COMPILER=$(which gcc) -DCMAKE_INSTALL_PREFIX:PATH=$(readlink -e $(pwd)/..) .'
